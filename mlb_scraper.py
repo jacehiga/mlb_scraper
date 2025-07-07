7883a8f8dee07d7d3845e3bb81f8c2dc9c583f65
Jace Higa <JKHIGA@willamette.edu>
2025-07-06 18:21:00 -0700

    mlb_scraper.py

--- mlb_scraper.py
+++ mlb_scraper.py
@@ -17,7 +17,7 @@ def clean_final_score_keys(raw_json):
 
 def main():
     # Set the date (use today or hardcode for testing)
-    today = datetime.now().date()
+    (datetime.now() - timedelta(days=1)).date()
     
     # Prepare container for results
     all_games = []
